45b173795e8d81e1d8026f819270b8903f74baa9
Danilo Medeiros <danilobmedeiros@gmail.com>
2021-08-28 13:09:39 -0300

    Ajuste to index redirect
    try for heroku

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, jsonify,make_response,redirect
+from flask import Flask, request, Response, jsonify,make_response,redirect, render_template
 from dao.mongoflask import MongoJSONEncoder, ObjectIdConverter
 from api.photos import photos_api
 from api.comments import comments_api
@@ -7,8 +7,9 @@ from api.likes import likes_api
 from flask_cors import CORS
 import os
 
+app = Flask(__name__, static_folder="../frontend/dist", template_folder="../frontend/dist",static_url_path='/')
 
-app = Flask(__name__, static_folder='../frontend/dist',static_url_path='/')
+#app = Flask(__name__, static_folder='./frontend/dist',static_url_path='/')
 app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
@@ -22,9 +23,10 @@ app.register_blueprint(likes_api)
 
 @app.route('/')
 def index():
-    print('funciona5')
-    #return redirect('/gallery')
-    return app.send_static_file('index.html')
+    print('funciona11')
+    return render_template('/index.html')
+    #return redirect('/index.html')
+  #  return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
